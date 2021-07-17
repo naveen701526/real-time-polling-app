0887af3361dbf8838aa581226a3bdcc9c96e6715
naveen <neonew8984@gmail.com>
2021-07-17 12:06:34 +0530

    Pusher App: Router module and creating routes

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ const path = require('path');
 
 const app = express();
 
+const poll = require('./routes/poll');
+
 // set up your public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // enable cors
 app.use(cors());
 
+app.use('/poll', poll);
+
 // create a port number
 const PORT = 3000;
 
 // start the server
-
 app.listen(PORT, () => console.log(`Server Started on PORT... ${PORT}`));
